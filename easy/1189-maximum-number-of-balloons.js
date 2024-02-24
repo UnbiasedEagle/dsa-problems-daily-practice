a5e8db21488e9b1581bedee01a23570e923ec479
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-02-24 19:40:20 +0530

    Solve 1189. Maximum Number of Balloons

---
+++ easy/1189-maximum-number-of-balloons.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function (text) {
+  const balloonMap = {
+    b: 1,
+    a: 1,
+    l: 2,
+    o: 2,
+    n: 1,
+  };
+
+  const textMap = {};
+
+  for (const char of text) {
+    textMap[text] = (textMap[char] || 0) + 1;
+  }
+
+  let result = Infinity;
+
+  for (const key in balloonMap) {
+    if (!textMap[key]) return 0;
+
+    const count = Math.floor(textMap[key] / balloonMap[key]);
+
+    result = Math.min(result, count);
+  }
+
+  return result === Infinity ? 0 : result;
+};
