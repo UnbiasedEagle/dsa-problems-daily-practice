14946f4516cc79d1e747c40da46c2586be2451a0
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-03-05 20:55:46 +0530

    Solve 94. Binary Tree Inorder Traversal iteratively

--- easy/94-binary-tree-inorder-traversal.js
+++ easy/94-binary-tree-inorder-traversal.js
@@ -11,12 +11,31 @@
  * @return {number[]}
  */
 var inorderTraversal = function (root) {
-  if (!root) {
-    return [];
-  }
-  const temp1 = inorderTraversal(root.left);
-  const temp2 = [root.val];
-  const temp3 = inorderTraversal(root.right);
+  if (!root) return [];
+  const result = [];
 
-  return [...temp1, ...temp2, ...temp3];
+  const stack = [root];
+  const visited = new Set();
+  visited.add(root);
+
+  while (stack.length > 0) {
+    if (
+      stack[stack.length - 1].left &&
+      !visited.has(stack[stack.length - 1].left)
+    ) {
+      const node = stack[stack.length - 1];
+      stack.push(node.left);
+      visited.add(node.left);
+      continue;
+    }
+    const node = stack.pop();
+    result.push(node.val);
+
+    if (node.right && !visited.has(node.right)) {
+      stack.push(node.right);
+      visited.add(node.right);
+    }
+  }
+
+  return result;
 };
