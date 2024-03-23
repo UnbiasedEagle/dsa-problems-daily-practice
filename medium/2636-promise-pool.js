bd3095a86dc8d1dc8b63578dd3f1a98dae9e1160
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-03-23 17:33:27 +0530

    Solve 2636 Promise Pool

---
+++ medium/2636-promise-pool.js
@@ -0,0 +1,20 @@
+var promisePool = async function (functions, n) {
+  const waitingPromises = [];
+
+  const wrapperFunctions = functions.map((fn) => {
+    return async () => {
+      await fn();
+      const next = waitingPromises.shift();
+      if (next) {
+        await next();
+      }
+    };
+  });
+
+  const currentlyRunning = wrapperFunctions.slice(0, n);
+  waitingPromises.push(...wrapperFunctions.slice(n));
+
+  return Promise.all(currentlyRunning);
+};
+
+TrAQvZybEd99kqrp;
