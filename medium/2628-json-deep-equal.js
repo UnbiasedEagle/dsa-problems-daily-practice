e365bc44336a6ea4ce6b0a8c5bf517186c6df295
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-03-25 17:34:38 +0530

    Solve 2628 - JSON Deep Equal

---
+++ medium/2628-json-deep-equal.js
@@ -0,0 +1,36 @@
+var areDeeplyEqual = function (o1, o2) {
+  if (o1 === null || o2 === null) {
+    return o1 === o2;
+  }
+
+  if (typeof o1 !== typeof o2) {
+    return false;
+  }
+
+  if (typeof o1 !== 'object') {
+    return o1 === o2;
+  }
+
+  if (Array.isArray(o1) && Array.isArray(o2)) {
+    if (o1.length !== o2.length) return false;
+
+    for (let i = 0; i < o1.length; i++) {
+      if (!areDeeplyEqual(o1[i], o2[i])) return false;
+    }
+
+    return true;
+  }
+
+  const keys1 = Object.keys(o1);
+  const keys2 = Object.keys(o2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (!areDeeplyEqual(o1[key], o2[key])) return false;
+  }
+
+  return true;
+};
