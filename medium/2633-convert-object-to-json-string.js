91c08641256fd7a7bb61cd01e203b23a752eaeb5
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-03-26 21:29:35 +0530

    Solve 2633 - Convert Object to JSON String

---
+++ medium/2633-convert-object-to-json-string.js
@@ -0,0 +1,27 @@
+function jsonStringify(object) {
+  if (object === null) {
+    return 'null';
+  }
+
+  if (typeof object === 'string') {
+    return `"${object}"`;
+  }
+
+  if (typeof object === 'number' || typeof object === 'boolean') {
+    return object.toString();
+  }
+
+  if (Array.isArray(object)) {
+    return '[' + object.map(jsonStringify).join(',') + ']';
+  }
+
+  if (typeof object === 'object') {
+    return `{${Object.keys(object)
+      .map((key) => {
+        return `${key}:${jsonStringify(object[key])}`;
+      })
+      .join(',')}}`;
+  }
+
+  return '';
+}
